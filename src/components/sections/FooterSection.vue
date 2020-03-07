6d61cdf900d988b8658733842096cae08f11c930
John Lowther <jw.lowther@hotmail.co.uk>
2020-03-07 15:17:52 +0000

    Add info in footer

--- src/components/sections/FooterSection.vue
+++ src/components/sections/FooterSection.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="footer-section"></div>
+  <div class="footer-section">
+    <span class="powered-by">
+      Powered by Vue.js
+      <img src="@/assets/tech-icons/vue.svg" >
+    </span>
+    
+    <span class="copyright">
+      &copy; {{ new Date().getFullYear() }} - John Lowther
+    </span>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer-section {
   height: 200px;
   width: 100%;
   background-color: black;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.powered-by {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+  
+  > img {
+    margin-left: 5px;
+    height: 25px;
+  }
 }
 </style>
